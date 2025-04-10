3f093fe099fc0a6650a4ca12840958b72da6f2be
toan-du <toandu.business@gmail.com>
2025-04-10 22:47:56 +0700

    inport test_collect

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import Collect.test_collect
-import tests.test_control
 import pyblish_lite
-import pyblish.api
 import pyblish.util
 
 pyblish.util.publish()
 Collect.test_collect.yooo()
-#pyblish.api.register_plugin(tests.test_control.test_something)
 
 pyblish_lite.show()
