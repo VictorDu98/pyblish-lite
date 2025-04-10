17c1c9b46234a1cebdea14f22907297856b43f84
toan-du <toandu.business@gmail.com>
2025-04-10 22:44:44 +0700

    inport test_collect

---
+++ Collect/test_collect.py
@@ -0,0 +1,29 @@
+import os
+import pyblish.api as pyblish
+
+
+def yooo():
+    class MyCollector(pyblish.ContextPlugin):
+        """This plug-in identifies content and creates instances,alsooo hello """
+
+        order = pyblish.CollectorOrder
+
+        def process(self, context):
+            for folder in os.listdir("."):
+                if not folder.endswith("_asset"):
+                    continue
+                instance = context.create_instance(folder)
+                instance.data["family"] = "asset"
+
+    class ValidateContents(pyblish.InstancePlugin):
+      """Ensure rig has the appropriate object sets"""
+
+      order = pyblish.ValidatorOrder
+      families = ["asset"]
+
+      def process(self, instance):
+        assert "controls_SEL" in instance, "%s is missing a controls set" % instance
+        assert "pointcache_SEL" in instance, "%s is missing a pointcache set" % instance
+
+    for plugin in [MyCollector, ValidateContents]:
+        pyblish.register_plugin(plugin)
--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import Collect.test_collect
+import tests.test_control
 import pyblish_lite
+import pyblish.api
+import pyblish.util
+
+pyblish.util.publish()
+Collect.test_collect.yooo()
+#pyblish.api.register_plugin(tests.test_control.test_something)
+
 pyblish_lite.show()
