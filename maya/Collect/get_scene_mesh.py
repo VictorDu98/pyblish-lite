97977e79d78357fdf32a813775f819e2c15c7645
Du Văn Toàn <toandu.business@gmail.com>
2025-04-13 19:43:51 +0700

    init test qc

---
+++ maya/Collect/get_scene_mesh.py
@@ -0,0 +1,17 @@
+import os
+import pyblish.api as pyblish
+
+class getSceneMesh(pyblish.ContextPlugin):
+    """Gather all mesh from current maya scene"""
+    hosts = ["maya"]
+    order = pyblish.CollectorOrder
+
+    def process(self, context):
+
+        from maya import cmds
+        shapes= cmds.ls(g=1)
+        assert len(shapes)>0,"No mesh!"
+        for shape in shapes:
+            instance = context.create_instance(shape)
+            instance.data["family"] = "mesh"
+
---
+++ maya/Validate/print_instances.py
@@ -0,0 +1,11 @@
+import os
+import pyblish.api as pyblish
+
+
+class PrintInstances(pyblish.InstancePlugin):
+    hosts = ["maya"]
+    families = ["mesh"]
+    order = pyblish.ValidatorOrder
+
+    def process(self, instance):
+        print("Instance is: %s" % instance)
