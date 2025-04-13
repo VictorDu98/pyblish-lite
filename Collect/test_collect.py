29a7811acca4b2ebd323c492d3ae10d9e5f7cd9d
Du Văn Toàn <toandu.business@gmail.com>
2025-04-13 14:33:10 +0700

    modify main.py to register plugin directory

--- Collect/test_collect.py
+++ Collect/test_collect.py
@@ -2,28 +2,25 @@ import os
 import pyblish.api as pyblish
 
 
-def yooo():
-    class MyCollector(pyblish.ContextPlugin):
-        """This plug-in identifies content and creates instances,alsooo hello """
+class MyCollector(pyblish.ContextPlugin):
+    """This plug-in identifies content and creates instances,alsooo hello """
 
-        order = pyblish.CollectorOrder
+    order = pyblish.CollectorOrder
 
-        def process(self, context):
-            for folder in os.listdir("."):
-                if not folder.endswith("_asset"):
-                    continue
-                instance = context.create_instance(folder)
-                instance.data["family"] = "asset"
+    def process(self, context):
+        for folder in os.listdir("."):
+            if not folder.endswith("_asset"):
+                continue
+            instance = context.create_instance(folder)
+            instance.data["family"] = "asset"
 
-    class ValidateContents(pyblish.InstancePlugin):
-      """Ensure rig has the appropriate object sets"""
+class ValidateContents(pyblish.InstancePlugin):
+  """Ensure rig has the appropriate object sets"""
 
-      order = pyblish.ValidatorOrder
-      families = ["asset"]
+  order = pyblish.ValidatorOrder
+  families = ["asset"]
 
-      def process(self, instance):
-        assert "controls_SEL" in instance, "%s is missing a controls set" % instance
-        assert "pointcache_SEL" in instance, "%s is missing a pointcache set" % instance
-
-    for plugin in [MyCollector, ValidateContents]:
-        pyblish.register_plugin(plugin)
+  def process(self, instance):
+    self.log.info("Extracting: %s" % instance)
+    #assert "controls_SEL" in instance, "%s is missing a controls set" % instance
+    #assert "pointcache_SEL" in instance, "%s is missing a pointcache set" % instance
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import Collect.test_collect
 import pyblish_lite
 import pyblish.util
 
-pyblish.util.publish()
-Collect.test_collect.yooo()
 
+pyblish.api.register_plugin_path(r"D:\tools\pyblish-lite\Collect")
+pyblish.util.publish()
 pyblish_lite.show()
